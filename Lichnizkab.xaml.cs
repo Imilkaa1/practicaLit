e45d37164e561b4d89a781b06a84dbfd9cc28f39
galashin <galashin78@Yandex.ru>
2024-02-16 11:28:31 +0400

    Авторизация, регистрация

---
+++ Lichnizkab.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica_Gataullov
+{
+    /// <summary>
+    /// Логика взаимодействия для Lichnizkab.xaml
+    /// </summary>
+    public partial class Lichnizkab : Window
+    {
+        public Lichnizkab()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ resistr.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Practica_Gataullov
+{
+    /// <summary>
+    /// Логика взаимодействия для resistr.xaml
+    /// </summary>
+    public partial class resistr : Window
+    {
+        public resistr()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resistr form2 = new Vxod();
+            form2.Show();
+        }
+    }
+}
